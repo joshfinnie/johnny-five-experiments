d30e18e3036551eaa89626c1957c0161f3c1072a
Josh Finnie <josh.finnie@trackmaven.com>
2016-07-30 14:05:14 -0400

    adding music, changing name of sentiment app.

---
+++ music.js
@@ -0,0 +1,28 @@
+var five = require('johnny-five');
+var songs = require('j5-songs');
+
+var board = new five.Board();
+
+board.on('ready', function () {
+    var piezo = new five.Piezo(3);
+    var button = new five.Button(2);
+
+    board.repl.inject({
+        button: button
+    });
+
+    button.on("down", function() {
+        if(!piezo.isPlaying) {
+            // piezo.play(songs.load('never-gonna-give-you-up'));
+            piezo.play(songs.load('mario-intro'), function(){
+                piezo.play(songs.load('mario-fanfare'));
+            });
+        }
+    })
+
+    if(!piezo.isPlaying) {
+        temperature.on("change", function() {
+            console.log(this.celsius + "°C", this.fahrenheit + "°F");
+        });
+    }
+});
