fbb00030b22176db342d7732544847126468d668
Josh Finnie <josh.finnie@trackmaven.com>
2016-07-30 16:14:38 -0400

    making note of where potentiometer needs to be...

--- music.js
+++ music.js
@@ -3,22 +3,22 @@ var songs = require('j5-songs');
 
 var board = new five.Board();
 var songList = [
-    "beethovens-fifth",
-    "claxon",
-    "do-re-mi",
-    "doorbell",
-    "funeral-march-short",
-    "jingle-bells-short",
-    "jingle-bells",
-    "mario-fanfare",
-    "mario-intro",
-    "never-gonna-give-you-up",
-    "nyan-intro",
-    "nyan-melody",
-    "pew-pew-pew",
-    "starwars-theme",
-    "tetris-theme",
-    "wedding-march"
+    "beethovens-fifth", // 0-63
+    "claxon", // 64-127
+    "do-re-mi", // 128-191
+    "doorbell", // 192-255
+    "funeral-march-short", // 256-319
+    "jingle-bells-short", // 320-383
+    "jingle-bells", // 384-447
+    "mario-fanfare", // 448-511
+    "mario-intro", // 512-575
+    "never-gonna-give-you-up", // 576-639
+    "nyan-intro", // 640-703
+    "nyan-melody", // 704-767
+    "pew-pew-pew", // 768-831
+    "starwars-theme", // 832-895
+    "tetris-theme", // 896-959
+    "wedding-march" // 960-1023
 ]
 
 board.on('ready', function () {
