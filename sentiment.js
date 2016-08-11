ba25c790d29a67532520f0a433e8084c53c17526
Josh Finnie <josh@jfin.us>
2016-08-11 11:42:50 -0400

    Adding comments to help understand what's going on here...

--- sentiment.js
+++ sentiment.js
@@ -1,17 +1,23 @@
+// adds .env file to Environment... awesome package!
 require('dotenv').config();
 
 var five = require("johnny-five");
 var sentiment = require('sentiment');
 var Twitter = require('twitter');
- 
+
+// Initalize board
 var board = new five.Board();
+
+// Initalize Twitter Client
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
- 
+
+// Looking for a command line variable
+// i.e. $ node sentiment.js "NodeBotsDay"
 var args = process.argv.slice(2);
 if(!args[0]){
     console.log("Need a command line variable!");
@@ -19,6 +25,7 @@ if(!args[0]){
 }
 
 board.on("ready", function (){
+    // Initialize the LED using the correct pins...
     var anode = new five.Led.RGB({
         pins: {
             red: 11,
@@ -26,7 +33,11 @@ board.on("ready", function (){
             blue: 9
         },
     });
+    
+    // Create a stream of tweets based on user inputed search term.
     client.stream('statuses/filter', {track: args[0]},  function(stream) {
+        
+        // On tweet, calculate sentiment and update LED color.
         stream.on('data', function(tweet) {
             var color = "#000000";
             var sentimentScore = (sentiment(tweet.text).score);
@@ -41,6 +52,7 @@ board.on("ready", function (){
             anode.color(color);
         });
 
+        // Catch errors... what errors?
         stream.on('error', function(error) {
             console.log(error);
         });
